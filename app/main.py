fec80e22d46ab021d9c562f7b8c3b9030b500a39
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-05 06:27:11 -0400

    Register service auth and recovery layer

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ import app.models.command_history  # noqa: F401
 import app.models.approval  # noqa: F401
 import app.models.scheduled_job  # noqa: F401
 import app.models.event_delivery  # noqa: F401
+import app.models.security_resilience  # noqa: F401
 from app.api.routes import router
 from app.api.control_center_routes import router as control_center_router
 from app.api.gateway_routes import router as gateway_router
@@ -28,6 +29,7 @@ from app.api.audit_command_routes import router as audit_command_router
 from app.api.approval_routes import router as approval_router
 from app.api.scheduler_routes import router as scheduler_router
 from app.api.event_delivery_routes import router as event_delivery_router
+from app.api.recovery_routes import router as recovery_router
 from app.core.config import settings
 from app.db.base import Base
 from app.db.session import engine
@@ -64,3 +66,4 @@ app.include_router(audit_command_router)
 app.include_router(approval_router)
 app.include_router(scheduler_router)
 app.include_router(event_delivery_router)
+app.include_router(recovery_router)
