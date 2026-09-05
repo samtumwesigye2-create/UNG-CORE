eacfdc4acd78bca6ece37f4c83ce5f86c842d8d6
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-05 00:45:34 -0400

    Configure automatic CORE health polling

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     iam_base_url: str = "http://ung-iam:8000"
     data_relay_base_url: str = "http://data-relay:8000"
     service_version: str = "0.1.0"
+    health_poll_enabled: bool = True
+    health_poll_interval_seconds: int = 30
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
