781c21b9faf9fc446dd0509b87bab11013b84e8b
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-05 06:14:36 -0400

    Register event delivery models and APIs

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ import app.models.alerting  # noqa: F401
 import app.models.command_history  # noqa: F401
 import app.models.approval  # noqa: F401
 import app.models.scheduled_job  # noqa: F401
+import app.models.event_delivery  # noqa: F401
 from app.api.routes import router
 from app.api.control_center_routes import router as control_center_router
 from app.api.gateway_routes import router as gateway_router
@@ -26,6 +27,7 @@ from app.api.alerting_routes import router as alerting_router
 from app.api.audit_command_routes import router as audit_command_router
 from app.api.approval_routes import router as approval_router
 from app.api.scheduler_routes import router as scheduler_router
+from app.api.event_delivery_routes import router as event_delivery_router
 from app.core.config import settings
 from app.db.base import Base
 from app.db.session import engine
@@ -61,3 +63,4 @@ app.include_router(alerting_router)
 app.include_router(audit_command_router)
 app.include_router(approval_router)
 app.include_router(scheduler_router)
+app.include_router(event_delivery_router)
