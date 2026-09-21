9b6549c2ba962551b1ba864b0e777e271994ec27
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-21 05:07:29 -0400

    Export logistic regression service

--- app/services/ml/__init__.py
+++ app/services/ml/__init__.py
@@ -1,3 +1,4 @@
 from app.services.ml.linear_regression import LinearRegressionResult, train_linear_regression
 
-__all__ = ["LinearRegressionResult", "train_linear_regression"]
+__all__ = ["LinearRegressionResult", "train_linear_regression", "LogisticRegressionResult", "train_logistic_regression"]
+from app.services.ml.logistic_regression import LogisticRegressionResult, train_logistic_regression\n
