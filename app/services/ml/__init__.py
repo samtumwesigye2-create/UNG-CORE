fd73ba66f66baa1054b9058ee790a3810f242a8b
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-21 05:07:46 -0400

    Add logistic regression classification
    * Add app/services/ml/logistic_regression.py
    
    * Add tests/test_ml_logistic_regression.py
    
    * Expose logistic regression API
    
    * Export logistic regression service
    
    * Clean ML service exports

--- app/services/ml/__init__.py
+++ app/services/ml/__init__.py
@@ -1,3 +1,9 @@
 from app.services.ml.linear_regression import LinearRegressionResult, train_linear_regression
+from app.services.ml.logistic_regression import LogisticRegressionResult, train_logistic_regression
 
-__all__ = ["LinearRegressionResult", "train_linear_regression"]
+__all__ = [
+    "LinearRegressionResult",
+    "train_linear_regression",
+    "LogisticRegressionResult",
+    "train_logistic_regression",
+]
