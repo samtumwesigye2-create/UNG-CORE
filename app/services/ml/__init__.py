9a7d9bdac5189046e18c883af1208008cffeb57d
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-21 05:11:01 -0400

    Export anomaly detection service

--- app/services/ml/__init__.py
+++ app/services/ml/__init__.py
@@ -1,9 +1,21 @@
 from app.services.ml.linear_regression import LinearRegressionResult, train_linear_regression
 from app.services.ml.logistic_regression import LogisticRegressionResult, train_logistic_regression
+from app.services.ml.anomaly_detection import (
+    AnomalyBaseline,
+    AnomalyDetectionResult,
+    detect_anomalies,
+    fit_anomaly_baseline,
+    score_anomalies,
+)
 
 __all__ = [
     "LinearRegressionResult",
     "train_linear_regression",
     "LogisticRegressionResult",
     "train_logistic_regression",
+    "AnomalyBaseline",
+    "AnomalyDetectionResult",
+    "fit_anomaly_baseline",
+    "score_anomalies",
+    "detect_anomalies",
 ]
